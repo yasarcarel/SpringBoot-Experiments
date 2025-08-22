3655dd0656072a982e4acf082fa1b62870365935
Yaşar Çarel <yasar.carel@gmail.com>
2025-08-22 16:25:51 +0300

    Java HotSpot Error Debugging

--- src/main/java/com/yasarcarel/Application.java
+++ src/main/java/com/yasarcarel/Application.java
@@ -3,7 +3,11 @@ package com.yasarcarel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Security Inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class Application {
 
 	public static void main(String[] args) {
