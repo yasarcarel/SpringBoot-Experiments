e49f0c74c8b89c46ca0da66598f493c63c6c98fd
Yaşar Çarel <yasar.carel@gmail.com>
2025-08-26 13:54:49 +0300

    DevTools Activation

---
+++ src/main/java/com/yasarcarel/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.yasarcarel.utils;
+
+public class ConfigProgramming {
+    //    DevTools ==> LiveReload
+    //    file ==> settings ==> compiler ==> Build project automatically(OK clicked) (Ctrl+Alt+S)
+    //    file ==> Advanced Settings ==>Allow Auto-make to start even (OK clicked)
+}
